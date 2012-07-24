0c2556c2d31db95abd89bf9972d84b24e54e8258
Tsutomu Kawamura <kawamura@cognitom.com>
2012-07-25 06:17:21 +0900

    First commit.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ wagon
 =====
 
 Embed images into the CSS on the fly.
+
+See also...
+
+* [CSS内の画像埋込で高速化(Data URI)](http://qiita.com/items/3134467210e0022f4a02)
---
+++ style/embed-images.php
@@ -0,0 +1,12 @@
+<?php
+$file = $_SERVER['DOCUMENT_ROOT'] . str_replace('?inline', '', $_SERVER['REQUEST_URI']);
+$css = file_get_contents($file);
+$css = preg_replace_callback('/url\((.*?)\)/im', function($matches){
+	$url = trim($matches[1], '\'\"');
+	return file_exists($url)
+		? 'url(data:image/png;base64,' . base64_encode(file_get_contents($url)) . ')'
+		: '';
+}, $css);
+
+header("Content-type: text/css");
+echo $css;
